87958a504b873b24dbe8522bec4493ddde7e30a2
Krishnasai Bharadwaj Atmakuri <34579499+AKSB0567@users.noreply.github.com>
2024-11-03 22:43:39 -0600

    Main and requirements

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+from routes.routes import register_routes
+
+app = Flask(__name__)
+
+# MongoDB connection details
+app.config["MONGO_URI"] = "mongodb+srv://bka2bg:QcqSyZpNSyiH52cU@crisismanagement.vypxy.mongodb.net/Crisis_Management"
+mongo = PyMongo(app)
+
+register_routes(app, mongo)
+
+if __name__ == "__main__":
+    app.run(debug=True)
