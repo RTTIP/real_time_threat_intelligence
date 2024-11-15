09eb59ca4307ca6fa59f38a39e97c35648ef1051
Vineeth Chava <25173696+vineethchava@users.noreply.github.com>
2024-11-14 23:25:20 -0600

    Delete ThreatIntelPlatform directory

--- ThreatIntelPlatform/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:12345@localhost:5432/threatintel')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
