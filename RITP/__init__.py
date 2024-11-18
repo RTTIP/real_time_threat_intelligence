3a5bf53dfabacb6f7068cdb65e951819b880bf64
Nikhileshcv <94368400+Nikhileshcv@users.noreply.github.com>
2024-11-18 15:23:29 -0600

    added llm integration feature

--- RITP/__init__.py
+++ RITP/__init__.py
@@ -1,26 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from apscheduler.schedulers.background import BackgroundScheduler
 
 db = SQLAlchemy()
-
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:nikhilesh@localhost/asset_management_database'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     db.init_app(app)  # Initialize the db with the app
 
     with app.app_context():
         db.create_all()  # Create tables within the app context
+
     from RITP.routes.crud_operations import crud_bp
     from RITP.routes.predict_impact import predict_bp
     from RITP.routes.read_threat import threat_bp
     from RITP.routes.llm_integration import llm_bp
-    # Route definitions
+    from RITP.routes.automated_incident import incident_bp
+    from RITP.routes.automated_incident import monitor_risks
     app.register_blueprint(crud_bp)
     app.register_blueprint(predict_bp)
     app.register_blueprint(threat_bp)
     app.register_blueprint(llm_bp)
+    app.register_blueprint(incident_bp)
+
+    def scheduled_task():
+        with app.app_context():
+            monitor_risks()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=scheduled_task, trigger="interval", seconds=3)
+    scheduler.start()
 
     return app
---
+++ RITP/routes/automated_incident.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, jsonify, current_app, Flask
+from RITP.Models import AssetRisk
+
+incident_bp = Blueprint("incident_bp", __name__)
+
+def monitor_risks():
+        high_risk_threshold = 90.00
+        critical_assets = AssetRisk.query.filter_by(threat_level='high').all()
+        for asset in critical_assets:
+            risk = AssetRisk.query.filter_by(asset_id=asset.asset_id).order_by(AssetRisk.risk_id.desc()).first()
+            if risk and risk.risk_score >= high_risk_threshold:
+                trigger_incident_response(asset, risk)
+
+def trigger_incident_response(asset, risk):
+    print(f"Incident Triggered for {asset.asset_id} due to high risk score: {risk.risk_score}")
+
+@incident_bp.route('/initiate_monitoring', methods=['GET'])
+def initiate_monitoring():
+    # Use app context to call monitor_risks directly
+
+    monitor_risks()  # Trigger the function immediately for manual testing
+    return jsonify({"status": "Monitoring started"}), 200
